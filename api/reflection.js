4121d7770c5d628fb5d35fb9c4da6af2be5cdfd3
Hernando Duarte <Hernando.Duarte@coherent.com>
2025-05-01 12:29:42 +0200

    adiciono relexion dinamica API y reflexiones.html

---
+++ api/reflection.js
@@ -0,0 +1,5 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    reflection: "María nos enseña a decir 'Fiat' a la voluntad de Dios. Reflexiona: ¿Cómo puedes confiar más en Él hoy?"
+  });
+}
